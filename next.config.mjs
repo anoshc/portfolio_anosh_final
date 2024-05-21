65fa0723dab6b53ae679b234c736b332eee652cf
Anosh Chaudhry <anosh@digitalinnlandet.no>
2024-05-21 12:36:13 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/portfolio_anosh_final" : "",
+  assetPrefix: isProd ? "/portfolio_anosh_final/" : "",
 };
 
 export default nextConfig;
